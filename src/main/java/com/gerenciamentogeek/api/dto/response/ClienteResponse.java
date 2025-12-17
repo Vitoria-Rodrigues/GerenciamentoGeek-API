ebc69ec7da5be9518997910ea5f68930249748a5
Vitória Rodrigues Ferreira <vitoria.rf@outlook.com>
2025-12-17 19:19:24 -0300

    Feat: adicionar DTO de resposta ClienteResponse

---
+++ src/main/java/com/gerenciamentogeek/api/dto/response/ClienteResponse.java
@@ -0,0 +1,60 @@
+package com.gerenciamentogeek.api.dto.response;
+
+public class ClienteResponse {
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String sexo;
+    private String telefone;
+
+    public ClienteResponse() {
+    }
+
+    public ClienteResponse(Long id, String nome, String cpf, String sexo, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.sexo = sexo;
+        this.telefone = telefone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
