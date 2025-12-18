9391df33bc52128bdb33d3b942476a1de9231a88
Vitória Rodrigues Ferreira <vitoria.rf@outlook.com>
2025-12-18 19:41:32 -0300

    Feat: adicionar idFuncionario ao LoginResponse

--- src/main/java/com/gerenciamentogeek/api/dto/response/LoginResponse.java
+++ src/main/java/com/gerenciamentogeek/api/dto/response/LoginResponse.java
@@ -1,15 +1,25 @@
 package com.gerenciamentogeek.api.dto.response;
 
 public class LoginResponse {
+    private Long idFuncionario;
     private String login;
     private String cargo;
 
     public LoginResponse() {
     }
 
-    public LoginResponse(String login, String cargo) {
+    public LoginResponse(Long idFuncionario, String login, String cargo) {
+        this.idFuncionario = idFuncionario;
         this.login = login;
         this.cargo = cargo;
+    }
+
+    public Long getIdFuncionario() {
+        return idFuncionario;
+    }
+
+    public void setIdFuncionario(Long idFuncionario) {
+        this.idFuncionario = idFuncionario;
     }
 
     public String getLogin() {
