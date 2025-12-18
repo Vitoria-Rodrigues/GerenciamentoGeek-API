f0830a68a6d417225e7a4c3e9eaa1855d771fa59
Vitória Rodrigues Ferreira <vitoria.rf@outlook.com>
2025-12-18 20:03:17 -0300

    Feat: adicionar controller da Venda com endpoints e respostas planejadas

---
+++ src/main/java/com/gerenciamentogeek/api/controllers/VendaController.java
@@ -0,0 +1,67 @@
+package com.gerenciamentogeek.api.controllers;
+
+import com.gerenciamentogeek.api.dto.response.Response;
+import com.gerenciamentogeek.api.dto.response.VendaResponse;
+import com.gerenciamentogeek.api.dto.vendaDTO;
+import com.gerenciamentogeek.api.services.VendaService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/vendas")
+public class VendaController{
+    private final VendaService vendaService;
+
+    public VendaController(VendaService vendaService) {
+        this.vendaService = vendaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Response<VendaResponse>> salvar(@RequestBody @Valid vendaDTO vendaDTO) {
+        try {
+            VendaResponse vendaResponse = vendaService.cadastrarVenda(vendaDTO);
+
+            Response<VendaResponse> response = new Response<>(
+                    "Sucesso",
+                    "Venda cadastrada com sucesso!",
+                    vendaResponse
+            );
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+        } catch (Exception e) {
+            Response<VendaResponse> response = new Response<>(
+                    "Erro",
+                    "Erro ao cadastrar venda: " + e.getMessage()
+            );
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+    }
+
+    @GetMapping({"/{cpfC", "/"})
+    public ResponseEntity<Response<List<VendaResponse>>> listarVendaPorCPFCliente(@PathVariable(required = false) String cpfC) {
+        try {
+            List<VendaResponse> vendaResponse = vendaService.listarVendas(cpfC);
+
+            Response<List<VendaResponse>> response = new Response<>(
+                    "Sucesso",
+                    "Venda encontrada com sucesso!",
+                    vendaResponse
+            );
+
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            Response<List<VendaResponse>> response = new Response<>(
+                    "Erro",
+                    e.getMessage(),
+                    null
+            );
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+    }
+
+}
