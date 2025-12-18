7639a9e779537cf270ec6aa1353b706cf7ac3be2
Vitória Rodrigues Ferreira <vitoria.rf@outlook.com>
2025-12-18 20:03:41 -0300

    Feat: adicionar configurações de segurança

---
+++ src/main/java/com/gerenciamentogeek/api/security/SecurityConfigurations.java
@@ -0,0 +1,25 @@
+package com.gerenciamentogeek.api.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers(HttpMethod.POST, "/login").permitAll();
+                    req.anyRequest().permitAll();
+                })
+                .build();
+    }
+}
